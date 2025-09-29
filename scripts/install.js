20ce44d6e9ce26c0d8cd4850f60984fd37294542
o0o52lin <345399240@qq.com>
2025-09-29 18:14:55 +0800

    Update install.js

--- scripts/install.js
+++ scripts/install.js
@@ -4,13 +4,16 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const pkgRoot = path.dirname(path.dirname(fileURLToPath(import.meta.url)));
+const myRoot = path.dirname(path.dirname(pkgRoot));
+const findpath = path.join(myRoot, 'frida_binding.node');
 const binding = path.join(pkgRoot, 'build', 'frida_binding.node');
-if (fs.existsSync(binding)) {
+if (fs.existsSync(findpath)) {
+  fs.copyFileSync(findpath, binding)
   process.exit(0);
 }
 
 try {
-  execSync('prebuild-install', { stdio: 'inherit' });
+  execSync('prebuild-install --verbose', { stdio: 'inherit' });
   process.exit(0);
 } catch (e) {
 }
